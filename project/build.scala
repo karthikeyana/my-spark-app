d247382bbb3086fbc8ae792b5a8979b0708e5dbc
karthikeyana <karthikeyan.a@live.com>
2017-08-31 17:56:29 +0530

    spark wordcount program implemented and build.scala file added

---
+++ project/build.scala
@@ -0,0 +1,53 @@
+import sbt._
+import Keys._
+import org.scalatra.sbt._
+import org.scalatra.sbt.PluginKeys._
+import com.earldouglas.xwp.JettyPlugin
+import com.mojolly.scalate.ScalatePlugin._
+import ScalateKeys._
+
+object MySparkAppServerBuild extends Build {
+  val Organization = "com.learning"
+  val Name = "My Spark App Server"
+  val Version = "0.1.0-SNAPSHOT"
+  val ScalaVersion = "2.11.8"
+  val ScalatraVersion = "2.5.1"
+  val SparkVersion = "2.2.0"
+
+  lazy val project = Project (
+    "my-spark-app-server",
+    file("."),
+    settings = ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+      organization := Organization,
+      name := Name,
+      version := Version,
+      scalaVersion := ScalaVersion,
+      resolvers += Classpaths.typesafeReleases,
+      resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
+      resolvers += Resolver.mavenLocal,
+      libraryDependencies ++= Seq(
+        "junit" % "junit" % "4.12" % "test",
+        "org.scalatra" %% "scalatra" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+        "org.apache.spark" %% "spark-core" % SparkVersion,
+        "com.sun.jersey" % "jersey-bundle" % "1.19.2"
+      ),
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
+        Seq(
+          TemplateConfig(
+            base / "webapp" / "WEB-INF" / "templates",
+            Seq.empty,  /* default imports should be added here */
+            Seq(
+              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
+            ),  /* add extra bindings here */
+            Some("templates")
+          )
+        )
+      }
+    )
+  ).enablePlugins(JettyPlugin)
+}
--- src/main/scala/com/spark/app/MySparkAppServlet.scala
+++ src/main/scala/com/spark/app/MySparkAppServlet.scala
@@ -1,6 +1,7 @@
 package com.spark.app
 
 import org.scalatra._
+import org.apache.spark.{ SparkContext, SparkConf }
 
 class MySparkAppServlet extends MySparkAppStack {
 
@@ -12,5 +13,16 @@ class MySparkAppServlet extends MySparkAppStack {
       </body>
     </html>
   }
+
+  get("/wc") {
+		val inputFile = "/home/limitless/Documents/projects/test/my-spark-app/README.md"
+		val outputFile = "/home/limitless/Documents/projects/test/my-spark-app/README.txt"
+		val conf = new SparkConf().setAppName("wordCount").setMaster("local[*]")
+		val sc = new SparkContext(conf)
+		val input =  sc.textFile(inputFile)
+		val words = input.flatMap(line => line.split(" "))
+		val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
+		counts.saveAsTextFile(outputFile)
+  	}
 
 }
